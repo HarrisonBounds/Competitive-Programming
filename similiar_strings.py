38988dd5b2cdced4b8d23342abd7f5ecfba7d590
Harrison Bounds <harrison.bounds777@gmail.com>
2022-10-06 09:58:57 -0500

    similiar strings

---
+++ similiar_strings.py
@@ -0,0 +1,34 @@
+s = ["aabbaac", "lkmn"]
+t = ["baacccb", "dddd"]
+arr= []
+
+for i in range(len(s)):
+    ds = {}
+    dt = {}
+    difference = 0
+    for j in range(len(s[i])):
+        if s[i][j] not in ds:
+            ds[s[i][j]] = 1
+        else:
+            ds[s[i][j]] = ds.get(s[i][j]) + 1
+
+    for k in range(len(s[i])):
+        if t[i][k] not in dt:
+            dt[t[i][k]] = 1
+        else:
+            dt[t[i][k]] = dt.get(t[i][k]) + 1
+
+    
+    for x in range(len(s[i])):
+        if t[i][x] in ds:
+            difference = abs(ds[t[i][x]] - dt[t[i][x]])
+        else:
+            difference += 1
+
+    if difference > 3:
+        arr.append("NO")
+    else:
+        arr.append("YES")
+
+print(arr)
+
